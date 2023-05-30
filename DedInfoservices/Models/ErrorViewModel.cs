629c98e07351a7d00027e96d4ead0e8c365b42e9
deivissonss <deivisson.santos@detran.ba.gov.br>
2023-05-30 14:23:36 -0300

    [FEATURE] commit inicial

---
+++ DedInfoservices/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DedInfoservices.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
